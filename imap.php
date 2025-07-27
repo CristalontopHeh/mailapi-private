06cfe8a670c3bb89c7bc031943bf7df9c26f83ad
CristalontopHeh <cristalo9283@gmail.com>
2025-07-27 23:06:23 +0200

    Update imap.php

--- imap.php
+++ imap.php
@@ -2,31 +2,29 @@
 include 'send_to_discord.php';
 
 function fetchEmails() {
-    $hostname = '{imap.mail.migadu.com:993/imap/ssl}INBOX';
+    $hostname = '{imap.migadu.com:993/imap/ssl}INBOX';
     $username = 'admin@cristalmails.dedyn.io';
     $password = 'cristal-XIUZIX-XIAJDO-XOOAK';
 
-    $inbox = @imap_open($hostname, $username, $password) or die('❌ Connexion IMAP échouée.');
+    $inbox = @imap_open($hostname, $username, $password)
+        or die('❌ Connexion IMAP échouée : vérifie les identifiants.');
 
     $emails = imap_search($inbox, 'ALL');
     $output = [];
 
     if ($emails) {
         rsort($emails);
-
         foreach ($emails as $email_number) {
             $overview = imap_fetch_overview($inbox, $email_number, 0)[0];
             $body = imap_fetchbody($inbox, $email_number, 1.1);
             if (trim($body) === '') {
                 $body = imap_fetchbody($inbox, $email_number, 1);
             }
-
             $message = [
                 'from' => $overview->from,
                 'subject' => $overview->subject,
                 'body' => quoted_printable_decode($body)
             ];
-
             $output[] = $message;
             sendToDiscord($message);
         }
