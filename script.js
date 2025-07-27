e006ffbdd1b38503318d36e6c0e79a1b5689ed01
CristalontopHeh <cristalo9283@gmail.com>
2025-07-27 23:11:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-setInterval(fetchEmails, 10000); // 10 sec
+async function fetchEmails() {
+    const res = await fetch("?action=fetch");
+    const mails = await res.json();
+    const container = document.getElementById("mail-list");
+    container.innerHTML = "";
 
-function fetchEmails() {
-    fetch('index.php?action=fetch')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('mail-list').innerHTML = html;
-        })
-        .catch(error => console.error('Erreur fetch:', error));
+    mails.forEach(mail => {
+        const div = document.createElement("div");
+        div.className = "mail";
+        div.innerHTML = `
+            <h3>${mail.subject}</h3>
+            <p><strong>De :</strong> ${mail.from}</p>
+            <p>${mail.body.replace(/\n/g, "<br>")}</p>
+        `;
+        container.appendChild(div);
+    });
 }
+
+setInterval(fetchEmails, 10000);
+fetchEmails();
